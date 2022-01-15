c89580e27d750243f73a8e07b5a2a1ec6674816e
ma022800 <marco.vargas@intelbras.com.br>
2022-01-15 10:08:17 -0300

    criado o pacote da api-bank

---
+++ api-bank.go
@@ -0,0 +1,27 @@
+package m74bankAPI
+
+const (
+	versionPackage = "2022-01-15"
+	exemplebool    = true
+)
+
+//usado para dizer se está em modo producao ou desenvolvimento
+var isProd = false
+
+//GetVersion Get the number version of packet
+func GetVersion() string {
+	return versionPackage
+}
+
+//SetLocalVar Set variable test
+//default is false
+func SetIsProduction(isInProduction bool) {
+	isProd = isInProduction
+}
+
+//GetIsProduction Set variable Pruduction
+func GetIsProduction() bool {
+	return isProd
+}
+
+//------------------ FIM ARQUIVO GO --------------------------------------
