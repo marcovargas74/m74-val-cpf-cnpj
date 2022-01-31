b448e7e1f86d54071c5d21b2c9f71df3a6d134f1
ma022800 <marco.vargas@intelbras.com.br>
2022-01-31 14:46:25 -0300

    compilado codigo fonte para incluir no docker

--- src/bank/main.go
+++ src/bank/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/marcovargas74/m74-bank-api/src/account"
 	bank "github.com/marcovargas74/m74-bank-api/src/api-bank"
@@ -12,12 +12,11 @@ var isProduction = false
 func init() {
 	bank.SetIsProduction(isProduction)
 	account.CreateDB(false)
+
 }
 
 func main() {
-	fmt.Printf("======== API BANK Version %s isPruduction=%v\n", bank.GetVersion(), bank.GetIsProduction())
-
-	//account.StructAndJSON()
+	log.Printf("======== API BANK Version %s isPruduction=%v\n", bank.GetVersion(), bank.GetIsProduction())
 	bank.StartAPI("dev")
 
 }
