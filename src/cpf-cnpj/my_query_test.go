3a0133a0e06b94ed5932cb11459f5c0b7ef4d3c5
ma022800 <marco.vargas@intelbras.com.br>
2022-07-06 00:18:29 -0300

    incluido novas funcoes de check para testes

--- src/cpf-cnpj/my_query_test.go
+++ src/cpf-cnpj/my_query_test.go
@@ -12,7 +12,7 @@ const (
 	erroMsg = "Test fail got Value[%v], wait Value [%v]"
 )
 
-//CheckIfEqualString check if resulto is OK type string
+//CheckIfEqualString check if result is OK type string
 func CheckIfEqualString(t *testing.T, gotValue, waitValue string) {
 	t.Helper()
 	if gotValue != waitValue {
@@ -20,10 +20,42 @@ func CheckIfEqualString(t *testing.T, gotValue, waitValue string) {
 	}
 }
 
-//CheckIfEqualBool check if resulto is OK type BOOL
+//CheckIfEqualBool check if result is OK type BOOL
 func CheckIfEqualBool(t *testing.T, gotValue, waitValue bool) {
 	t.Helper()
 	if gotValue != waitValue {
+		t.Errorf(erroMsg, gotValue, waitValue)
+	}
+}
+
+//CheckIfEqualInt check if result is OK type INT
+func CheckIfEqualInt(t *testing.T, gotValue, waitValue uint64) {
+	t.Helper()
+	if gotValue != waitValue {
+		t.Errorf(erroMsg, gotValue, waitValue)
+	}
+}
+
+//CheckIfEqualFloat check if result is OK type FLOAT
+func CheckIfEqualFloat(t *testing.T, gotValue, waitValue float64) {
+	t.Helper()
+	if gotValue != waitValue {
+		t.Errorf(erroMsg, gotValue, waitValue)
+	}
+}
+
+//CheckIfEqualTime check if result is OK type Time
+func CheckIfEqualTime(t *testing.T, gotValue, waitValue time.Time) {
+	t.Helper()
+	if gotValue != waitValue {
+		t.Errorf(erroMsg, gotValue, waitValue)
+	}
+}
+
+//CheckIfUptimeIsOK check if result is OK to UpTime
+func CheckIfUptimeIsOK(t *testing.T, gotValue, waitValue float64) {
+	t.Helper()
+	if gotValue <= waitValue {
 		t.Errorf(erroMsg, gotValue, waitValue)
 	}
 }
