b42fcfca48ff01f352c4e0a0b5ffdfb0616a0ad9
ma022800 <marco.vargas@intelbras.com.br>
2022-07-06 00:22:03 -0300

    Criado testes para funcoes de status do app

---
+++ src/cpf-cnpj/status_query_test.go
@@ -0,0 +1,84 @@
+package cpfcnpj
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCreateStatus(t *testing.T) {
+
+	tests := []struct {
+		give                string
+		wantTotalQueryValue uint64
+	}{
+		{
+			give:                "Testa Se Criou Status Como Zero",
+			wantTotalQueryValue: 0,
+		},
+	}
+
+	CreateStatus()
+	for _, tt := range tests {
+
+		t.Run(tt.give, func(t *testing.T) {
+			CheckIfEqualInt(t, GetNumQuery(), tt.wantTotalQueryValue)
+		})
+
+	}
+
+}
+
+func TestUpdateStatus(t *testing.T) {
+
+	tests := []struct {
+		give                string
+		wantTotalQueryValue uint64
+	}{
+		{
+			give:                "Testa Se Incrementou o numero de consultas_1",
+			wantTotalQueryValue: 1,
+		},
+		{
+			give:                "Testa Se Incrementou o numero de consultas_2",
+			wantTotalQueryValue: 2,
+		},
+		{
+			give:                "Testa Se Incrementou o numero de consultas_3",
+			wantTotalQueryValue: 3,
+		},
+		{
+			give:                "Testa Se Incrementou o numero de consultas_4",
+			wantTotalQueryValue: 4,
+		},
+	}
+
+	CreateStatus()
+	for _, tt := range tests {
+
+		t.Run(tt.give, func(t *testing.T) {
+			UpdateStatus()
+			CheckIfEqualInt(t, GetNumQuery(), tt.wantTotalQueryValue)
+		})
+
+	}
+
+}
+
+func TestUpTimeStatus(t *testing.T) {
+
+	CreateStatus()
+	time.Sleep(3 * time.Second)
+	//UpdateStatus()
+	lastTime := GetUptimeQuery()
+	CheckIfUptimeIsOK(t, lastTime, 3)
+}
+
+// func TestShowStatus(t *testing.T) {
+
+// 	CreateStatus()
+
+// 	ShowStatus()
+// 	//UpdateStatus()
+// 	lastTime := GetUptimeQuery()
+// 	CheckIfUptimeIsOK(t, lastTime, 3)
+// }
