45138cb5ecb07d48e1ae121ac9f1761482a2ccf2
ma022800 <marco.vargas@intelbras.com.br>
2022-07-10 22:56:09 -0300

    retirado variavel nao usada

--- src/api-validator/api-validator.go
+++ src/api-validator/api-validator.go
@@ -4,9 +4,6 @@ const (
 	versionPackage = "2022-07-04"
 )
 
-//IsMongoDBI VAR used to choise BD
-var IsMongoDBI bool
-
 //GetVersion Get the number version of packet
 func GetVersion() string {
 	return versionPackage
