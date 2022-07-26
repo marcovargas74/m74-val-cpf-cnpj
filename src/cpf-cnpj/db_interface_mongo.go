6bd47481a36ca09cf233a327b8752c5cd1b12911
ma022800 <marco.vargas@intelbras.com.br>
2022-07-26 15:45:11 -0300

    criado uma camada para acessar o db

--- src/cpf-cnpj/db_interface_mongo.go
+++ src/cpf-cnpj/db_interface_mongo.go
@@ -2,14 +2,10 @@ package cpfcnpj
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"gopkg.in/mgo.v2/bson"
 )
 
 const (
@@ -55,131 +51,8 @@ func InitDBMongo(isDockerRun bool) bool {
 		return false
 	}
 
-	fmt.Println("connecting to the MONGO DB...")
 	collectionQuery = client.Database("Querys").Collection("Querys")
-	fmt.Println("Successfully connected to the DB MONGO!")
+	log.Println("Successfully connected to the DB MONGO!")
 	return true
 
-}
-
-func (q *MyQuery) saveQueryInMongoDB() error {
-
-	_, err := collectionQuery.InsertOne(ctx, q)
-	return err
-}
-
-func (q *MyQuery) showQueryAllMongoDB() (string, error) {
-
-	filter := bson.M{}
-
-	cursor, err := collectionQuery.Find(ctx, filter)
-	if err != nil {
-		return err.Error(), err
-	}
-	defer cursor.Close(ctx)
-
-	var queryList []MyQuery
-	for cursor.Next(ctx) {
-		var aQuery MyQuery
-		err := cursor.Decode(&aQuery)
-		if err != nil {
-			return err.Error(), err
-		}
-		queryList = append(queryList, aQuery)
-	}
-
-	if len(queryList) == 0 {
-		errEmpty := errors.New("MONGODB: is Empty")
-		return errEmpty.Error(), errEmpty
-	}
-
-	json, err := json.Marshal(queryList)
-	if err != nil {
-		return err.Error(), err
-	}
-
-	return string(json), nil
-}
-
-func (q *MyQuery) showQuerysByTypeMongoDB(isCPF bool) (string, error) {
-
-	filter := bson.M{"is_cpf": bson.M{"$eq": isCPF}}
-
-	cursor, err := collectionQuery.Find(ctx, filter)
-	if err != nil {
-		return err.Error(), err
-	}
-	defer cursor.Close(ctx)
-
-	var queryList []MyQuery
-	for cursor.Next(ctx) {
-		var aQuery MyQuery
-		err := cursor.Decode(&aQuery)
-		if err != nil {
-			return err.Error(), err
-		}
-		queryList = append(queryList, aQuery)
-	}
-
-	if len(queryList) == 0 {
-		errEmpty := errors.New("MONGODB: Not Found elements to this Type")
-		return errEmpty.Error(), errEmpty
-	}
-
-	json, err := json.Marshal(queryList)
-	if err != nil {
-		return err.Error(), err
-	}
-
-	return string(json), nil
-
-}
-
-func (q *MyQuery) deleteQuerysByNumMongoDB(findNum string) error {
-
-	filter := bson.M{"cpf": bson.M{"$eq": findNum}}
-
-	result, err := collectionQuery.DeleteOne(ctx, filter)
-	if err != nil {
-		return err
-	}
-
-	if result.DeletedCount == 0 {
-		return fmt.Errorf("num %q Not Found", findNum)
-	}
-
-	return nil
-}
-
-func (q *MyQuery) showQuerysByNumMongoDB(findNum string) (string, error) {
-
-	filter := bson.M{"cpf": bson.M{"$eq": findNum}}
-
-	cursor, err := collectionQuery.Find(ctx, filter)
-	if err != nil {
-		return err.Error(), err
-	}
-	defer cursor.Close(ctx)
-
-	var queryList []MyQuery
-	for cursor.Next(ctx) {
-		var aQuery MyQuery
-		err := cursor.Decode(&aQuery)
-		if err != nil {
-			return err.Error(), err
-		}
-		queryList = append(queryList, aQuery)
-	}
-
-	if len(queryList) == 0 {
-		errEmpty := fmt.Errorf("num %q Not Found", findNum)
-		return errEmpty.Error(), errEmpty
-	}
-
-	json, err := json.Marshal(queryList)
-	if err != nil {
-		return err.Error(), err
-	}
-
-	return string(json), nil
 }
